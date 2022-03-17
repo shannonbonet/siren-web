c36f84e9a1d140620703012cb11f8684353cd221
j-keating0002 <57604304+j-keating0002@users.noreply.github.com>
2022-03-16 19:05:16 -0700

    Update index.tsx
    just removed import statements that aren't currently being used

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link'
 import { ClientInfo } from '../../components/ClientInfo/ClientInfo'
-import Layout from '../../components/Layout'
 
 export default function IntakeDashboard() {
     return (
