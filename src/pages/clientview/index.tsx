d8e56d250740e59b15bde9b56dd43c7eb44f3080
bry-gavino <bry_gavino01@berkeley.edu>
2022-04-22 13:27:00 -0700

    deleted extra components folder and added layout navigation to client view

--- src/pages/clientview/index.tsx
+++ src/pages/clientview/index.tsx
@@ -1,11 +1,14 @@
 import { ClientInfo } from '../../components/ClientInfo/ClientInfo';
+import Layout from "../../components/Layout";
 
-export default function IntakeDashboard() {
+export default function ClientView() {
     return (
         // Layout wraps this component in index.tsx
         <div> 
-            <h1>Intake Dashboard</h1>
-            <ClientInfo/>
+            <Layout>
+                <h1>Intake Dashboard</h1>
+                <ClientInfo/>
+            </Layout>
         </div>
     )
 }
